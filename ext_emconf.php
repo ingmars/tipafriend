ef8931ad57041257046f35fb2df1a036250b35c9
Ingmar Schlecht <ingmar.schlecht@unibas.ch>
2013-09-23 20:03:40 +0200

    Initial commit of tipafriend version 1.2.4 from TER (without my fix)

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tipafriend".
+#
+# Auto generated 03-10-2011 04:30
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Tip-A-Friend',
+	'description' => 'Makes a link to a page where a mail form allows the user to send the link to the former page to some recipient email address. Mails can be HTML or plain text',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.2.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.1.0-0.0.0',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Kasper Skaarhoj',
+	'author_email' => 'kasperYYYY@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
